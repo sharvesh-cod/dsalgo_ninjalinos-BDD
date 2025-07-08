65e4c2bcdc983e92ce79adb654f4f67c8c8f66d4
sowmya <sowmya12345g@gmail.com>
2025-07-08 12:27:59 -0500

    pasted from chatgpt

--- src/test/java/utils/ConfigReader.java
+++ src/test/java/utils/ConfigReader.java
@@ -1,21 +1,22 @@
 package utils;
 
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.Properties;
 
 public class ConfigReader {
 
 	public void fileReader() throws IOException {
-
-//		FileInputStream  fis = new FileInputStream ("C:\\Testleaf-1\\Selenium\\Eclipse\\eclipse\\Ninjalinos\\src\\test\\resources\\config\\config.properties");
-//		Properties	prop = new Properties();
-//		prop.load(fis);
-//		System.out.println(prop.getProperty("broswer"));	
+		FileInputStream fis = new FileInputStream(
+				"C:\\Testleaf-1\\Selenium\\Eclipse\\eclipse\\Ninjalinos\\src\\test\\resources\\config\\config.properties");
+		Properties prop = new Properties();
+		prop.load(fis);
+		System.out.println(prop.getProperty("browser"));
 	}
 
 	public static void main(String[] args) throws IOException {
 		ConfigReader rf = new ConfigReader();
 		rf.fileReader();
-
 	}
 
 }
