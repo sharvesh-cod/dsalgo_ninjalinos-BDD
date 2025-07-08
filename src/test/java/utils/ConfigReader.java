bedeba95571fca3c0b85884904ecf27efa8cc895
sharvesh-cod <sharveshyuva5@gmail.com>
2025-07-08 11:16:18 -0400

    Updated changes

--- src/test/java/utils/ConfigReader.java
+++ src/test/java/utils/ConfigReader.java
@@ -1,19 +1,21 @@
 package utils;
 
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.Properties;
 
 public class ConfigReader {
 
-	private Properties prop;
+	public void fileReader() throws IOException {
 
-	public Properties initProp() throws IOException {
-		prop = new Properties();
-		FileInputStream fis = new FileInputStream(
-				"C:\\Testleaf-1\\Selenium\\Eclipse\\eclipse\\Ninjalinos\\src\\test\\resources\\config\\config.properties");
-		prop.load(fis);
-		return prop;
+//			FileInputStream  fis = new FileInputStream ("C:\\Testleaf-1\\Selenium\\Eclipse\\eclipse\\Ninjalinos\\src\\test\\resources\\config\\config.properties");
+//			Properties	prop = new Properties();
+//			prop.load(fis);
+//			System.out.println(prop.getProperty("broswer"));	
+	}
+
+	public static void main(String[] args) throws IOException {
+		ConfigReader rf = new ConfigReader();
+		rf.fileReader();
+
 	}
 
 }
