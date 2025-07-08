a6098c6be3a0d4cdae1f21293061a9a00cc140a3
sowmya <sowmya12345g@gmail.com>
2025-07-08 11:58:37 -0500

    updated confg

--- src/test/java/utils/ConfigReader.java
+++ src/test/java/utils/ConfigReader.java
@@ -6,11 +6,10 @@ public class ConfigReader {
 
 	public void fileReader() throws IOException {
 
-		// FileInputStream fis = new FileInputStream
-		// ("C:\\Testleaf-1\\Selenium\\Eclipse\\eclipse\\Ninjalinos\\src\\test\\resources\\config\\config.properties");
-		// Properties prop = new Properties();
-		// prop.load(fis);
-		// System.out.println(prop.getProperty("broswer"));
+//		FileInputStream  fis = new FileInputStream ("C:\\Testleaf-1\\Selenium\\Eclipse\\eclipse\\Ninjalinos\\src\\test\\resources\\config\\config.properties");
+//		Properties	prop = new Properties();
+//		prop.load(fis);
+//		System.out.println(prop.getProperty("broswer"));	
 	}
 
 	public static void main(String[] args) throws IOException {
