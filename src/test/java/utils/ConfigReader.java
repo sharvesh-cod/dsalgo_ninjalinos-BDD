0cef7637225ab01619848817ed48730534854d85
sharvesh-cod <sharveshyuva5@gmail.com>
2025-07-08 16:40:27 -0400

    commited the config reader

--- src/test/java/utils/ConfigReader.java
+++ src/test/java/utils/ConfigReader.java
@@ -5,17 +5,15 @@ import java.io.IOException;
 public class ConfigReader {
 
 	public void fileReader() throws IOException {
-
-//			FileInputStream  fis = new FileInputStream ("C:\\Testleaf-1\\Selenium\\Eclipse\\eclipse\\Ninjalinos\\src\\test\\resources\\config\\config.properties");
-//			Properties	prop = new Properties();
-//			prop.load(fis);
-//			System.out.println(prop.getProperty("broswer"));	
+//		FileInputStream  fis = new FileInputStream ("C:\\Testleaf-1\\Selenium\\Eclipse\\eclipse\\Ninjalinos\\src\\test\\resources\\config\\config.properties");
+//		Properties	prop = new Properties();
+//		prop.load(fis);
+//		System.out.println(prop.getProperty("broswer"));	
 	}
 
 	public static void main(String[] args) throws IOException {
 		ConfigReader rf = new ConfigReader();
 		rf.fileReader();
-
 	}
 
 }
