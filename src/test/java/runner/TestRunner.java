ced43179321312c0b1a34caf09cd4d53aee9ebe6
sharvesh-cod <sharveshyuva5@gmail.com>
2025-07-26 11:45:47 -0400

    updated excel data

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -6,9 +6,9 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(plugin = { "pretty", "com.aventstack.chaintest.plugins.ChainTestCucumberListener:",
-		"html:target/dsAlgo_Ninjalinos_BDD.html" }, // reporting purpose
-		monochrome = false, // console output color
-		tags = "@TreePracticeQuestionlink", features = { "src/test/resources/Feature" }, // location
+		"html:target/dsAlgo_Ninjalinos_BDD.html" }, tags = "@NinjalinosTree", // reporting purpose
+		monochrome = false, // console output color //tags = "@NinjalinosGraph",
+		features = { "src/test/resources/Feature" }, // location
 		// of
 		// feature files
 		glue = { "stepDefinition", "hooks" }) // location of step definition files
